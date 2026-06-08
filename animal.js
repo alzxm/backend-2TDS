e07e1c81ff2382ada5f1d1e5437099d39c0a9c8b
Alejandra M <brayandy.blanco@escola.pr.gov.br>
2026-06-08 13:49:29 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,10 +1,16 @@
 const animal = {
     nome: "Luciano",
-    especie: "Gato",
+    especie: "gato",
     idade: 1,
     tutor: false,
     vacinado: true
 };
 
-//console.log(animal)
+//console.log(animal);
 console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 1;
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com ${animal.idade} de idade e possui sua vacina em dia ${animal.vacinado}`)
