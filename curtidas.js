d9d17941a043c033c78bac407b116c2a542d6b15
Alejandra M <brayandy.blanco@escola.pr.gov.br>
2026-06-29 13:56:26 +0000

    v2

--- curtidas.js
+++ curtidas.js
@@ -2,7 +2,8 @@ const feedJSON = require('./feed.json');
 
 // Adicionar uma curtida ao post pelo id
 function curtir(feed, id) {
-    const post = feed[0].id === id ? feed[0];
+    const post = feed[0].id === id ? feed[0]:
+    feed[1].id === id ? feed[1] : feed[2]; //busca pelo id 
 }
 
 if (!post) {
@@ -17,3 +18,19 @@ if(!post.publicado) {
 
 post.metricas.curtidas++;
 console.log(`Post  de @${post.autor} agora tem ${post.metricas.curtidas} curtidas.`)
+
+// Remove uma curtida - não deixa ir abaixo de 0
+function descurtir(feed, id) {
+    const post = feed [0].id === id ? feed[0] :
+                feed[1].id === id ? feed[1] :  feed[2];
+}
+
+if(post.metricas.curtidas === 0) {
+    console.log("Este post já está com 0 curtidas.");
+    return
+}
+
+post.metricas.curtidas++;
+console.log(`💔 Curtidas removida. Total ${post.metricas.curtidas}`)
+
+curtir(feedJSON, 1);
